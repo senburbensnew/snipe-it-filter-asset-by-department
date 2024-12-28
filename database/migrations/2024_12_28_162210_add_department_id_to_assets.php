d78fcd8e6a85c3f1eca3195b2786ec07d7f46aca
pierre rubens milorme <prubensmilorme@gmail.com>
2024-12-28 18:47:44 -0500

    Initial commit

---
+++ database/migrations/2024_12_28_162210_add_department_id_to_assets.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+    Custom migration file to add department support to assets
+*/
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->unsignedInteger('department_id')->nullable();
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->dropForeign(['department_id']);
+            $table->dropColumn('department_id');    
+        });
+    }
+};
